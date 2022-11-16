0dfc094f7b658cebe2722355782f7dc84aff5b85
James Scott <jsa.codes@gmail.com>
2022-11-16 15:29:15 -0600

    Modified Subscription Model. follower is now set to ForeignKey instead of OneToOneField. Also indented the class definition in the other files to make it easier to read for myself.

--- lightningapi/models/subscription.py
+++ lightningapi/models/subscription.py
@@ -2,7 +2,9 @@ from django.db import models
 from .post import Post
 from .reaction import Reaction
 from django.contrib.auth.models import User
+
+
 class Subscription(models.Model):
-    follower = models.OneToOneField(User, on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE)
     author = models.OneToOneField(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField()
