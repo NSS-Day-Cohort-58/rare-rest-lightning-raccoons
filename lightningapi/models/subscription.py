deb5aecb523ff58cad49245cddf3c4cbb23f0d59
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-11-17 09:46:02 -0600

    Adding space

--- lightningapi/models/subscription.py
+++ lightningapi/models/subscription.py
@@ -2,6 +2,8 @@ from django.db import models
 from .post import Post
 from .reaction import Reaction
 from django.contrib.auth.models import User
+
+
 class Subscription(models.Model):
     follower = models.OneToOneField(User, on_delete=models.CASCADE)
     author = models.OneToOneField(User, on_delete=models.CASCADE)
